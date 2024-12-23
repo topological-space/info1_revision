b259b2950bd5f5047726d15d1df5729b282fa799
Your Name <you@example.com>
2024-12-23 15:52:16 +0100

    first commit

---
+++ algo6.py
@@ -0,0 +1,9 @@
+age = int(input("age: "))
+if age >= 21 and age <= 70:
+    print("mr7ba")
+elif age >= 18:
+    print("dkhl")
+elif age >= 12:
+    print("sir kbr")
+else:
+    print("tla7")
---
+++ algo7.py
@@ -0,0 +1,3 @@
+for i in range(100):
+    for j in range(100):
+        print(i, "x", j, sep="")
